66cb9aa632ad02e73b6f15bbd69ec494643668e4
Dasari <dasarirajesh@cse.iitb.ac.in>
2020-08-30 21:08:14 +0530

    update usage in main.cpp

--- main.cpp
+++ main.cpp
@@ -6,13 +6,15 @@ int main(){
 	int n;
 	cin>>n;
 	vector<int> v(n);
-	
+	int startidx,endidx;
+	cin>>startidx;
+	cin>>endidx;
 	for(int i =0 ; i<n ; i++){
 		cin>>v[i];
 	}
 	int num;
 	cin>>num;
-	if(search_custom(v,num))
+	if(search_custom(v,num,startidx,endidx))
 		cout<<"Found"<<endl;
 	else
 		cout<<"Not Found";
